a0780a896ba5274c3620b9de2209337b0bc1a6e6
Liao <80034561+YoungStudio708@users.noreply.github.com>
2022-01-24 18:30:45 +0300

    2

--- Postgre/Podd.trancating_Tables.sql
+++ Postgre/Podd.trancating_Tables.sql
@@ -1,1 +1,10 @@
+TRUNCATE TABLE nedvijimost;
 TRUNCATE TABLE otdel;
+TRUNCATE TABLE type_uslug;
+TRUNCATE TABLE client;
+TRUNCATE TABLE type_sdelki;
+TRUNCATE TABLE sotrudnik;
+TRUNCATE TABLE sdelka;
+TRUNCATE TABLE position_s;
+TRUNCATE TABLE sotrudnik_position;
+TRUNCATE TABLE sotrudnik_otdel;
