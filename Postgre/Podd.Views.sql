9113ad505f4865e9d2342b14d93df05f3a084c02
Liao <80034561+YoungStudio708@users.noreply.github.com>
2022-01-30 11:00:06 +0300

    13

--- Postgre/Podd.Views.sql
+++ Postgre/Podd.Views.sql
@@ -8,3 +8,15 @@ SELECT
     FROM sotrudnik_position
     INNER JOIN  sotrudnik ON sotrudnik_id = id_sotrudnik
     INNER JOIN  position_s ON position_id = id_position;
+
+
+CREATE OR REPLACE VIEW staff_rasp_view
+AS
+SELECT
+    CONCAT('Name of position', position_name, 'Service type name', type_uslug_name,
+            'department name', otdel_name)
+    FROM sotrudnik_position
+    INNER JOIN position_s ON position_id = id_position
+    INNER JOIN otdel ON otdel_id = id_otdel
+    INNER JOIN type_uslug ON type_uslug_id = id_type_uslug;
+    
