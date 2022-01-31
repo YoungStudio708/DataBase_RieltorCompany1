0af2ad5f3d9dd6c48a5547b9a1b03d1087856113
Liao <80034561+YoungStudio708@users.noreply.github.com>
2022-01-31 19:07:50 +0300

    123

--- Postgre/Podd.functions.sql
+++ Postgre/Podd.functions.sql
@@ -1,1 +1,27 @@
-CREATE OR REPLACE FUNCTION SalaryCalculatingForStaff (p_id_sotrudnik_position INT, )
+create or replace function SalariesForHoursWorked(p_id_sotrudnik_position int,
+										          p_WorkedHours decimal(38, 2))
+returns decimal(38, 2)
+language plpgsql
+as $$
+	declare CountMoney decimal(38, 2);
+	begin
+    	select sum(Salary * 0.87 * (p_WorkedHours / 160))
+    	into CountMoney from sotrudnik_position
+        where id_sotrudnik_position = p_id_sotrudnik_position;
+    	return CountMoney;
+	end;
+$$;
+
+create or replace function Premiya()
+returns int
+language plpgsql
+
+as $$
+    declare premia int;
+    begin
+        premia = sum(salary*2.00)
+        from sotrudnik
+        inner join position_s on position_id = id_position;
+return premia;
+end;
+$$;
