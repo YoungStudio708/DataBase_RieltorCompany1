566e868b9f6a75d8b11e3497c1050f7448c82d66
YoungStudio708 <zhilcove@gmail.com>
2022-02-03 23:43:23 +0300

    new file with views

---
+++ MySQL/rieltor.Views.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE VIEW workingbook_view
+AS
+    SELECT 
+     CONCAT(last_name, ' ', SUBSTRING(first_name, 1, 1), '.', SUBSTRING(otchestwo, 1, 1), '. ',
+            'Passport: ', passport_serija, '-', passport_number, '.') AS "Passport data",
+    CONCAT('Phone number: ', telephone_number, 
+            'Life address: ', life_address, '.') AS "Contact information"
+    FROM sotrudnik_position
+    INNER JOIN  sotrudnik ON sotrudnic_id = id_sotrudnik
+    INNER JOIN  position_s ON position_id = id_position;
+
+    CREATE OR REPLACE VIEW staff_rasp_view
+AS
+    SELECT
+    CONCAT('Name of position', position_name, 'Service type name', type_uslug_name,
+            'department name', otdel_name)
+    FROM sotrudnik_position
+    INNER JOIN position_s ON position_id = id_position
+    INNER JOIN otdel ON otdel_id = id_otdel
+    INNER JOIN type_uslug ON type_uslug_id = id_type_uslug;
