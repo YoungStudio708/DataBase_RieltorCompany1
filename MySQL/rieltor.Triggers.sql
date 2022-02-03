069b64b097defe9a37b8aaca3c40f24a1be2691c
YoungStudio708 <zhilcove@gmail.com>
2022-02-04 01:03:33 +0300

    1

--- MySQL/rieltor.Triggers.sql
+++ MySQL/rieltor.Triggers.sql
@@ -6,22 +6,15 @@ sotrudnik_info varchar(20) not null,
 post_info varchar(20) not null,
 create_date date null default now()
 );
-
-DELIMITER $$
-create TRIGGER history_insert
-    AFTER INSERT 
-    ON sotrudnik_position
-    for each row
-BEGIN 
-    INSERT INTO sotrudnik_history(status_rec, sotrudnik_info, post_info)
-    values ('insert', ((select concat('Insert new staff. ',
-    'Last name: ', cast(NEW.last_name as varchar(30)), ';',
-    'Name: ', cast(NEW.first_name as varchar(30)), ';',
-    'Patronymic: ', cast(NEW.otchestwo as varchar(30)), ';',
-    ) from sotrudnik
-    WHERE id_sotrudnik = NEW.sotrudnik_id),
-    (SELECT CONCAT('Position name: ',
-        cast(position_name as varchar(30)), ';')
-        FROM position_s WHERE id_position = NEW.position_id);
-END $$
-DELIMITER ;
+delimiter //
+create or replace trigger History_Insert /*создание триггера*/
+after insert on sotrudnik /*после ввода в ...*/
+for each row /*для каждой строки*/
+begin /*начало*/
+if NEW.Id_Sotrudnik then /*если NEW.ID_Employee тогда*/
+insert into sotrudnik_history(status_rec, sotrudnik_info, post_info) /*ввод в Employee_History*/
+VALUES('Новая запись', /*значения*/
+(select concat(last_name,' ',first_name,' ',otchestwo) from sotrudnik where id_sotrudnik = NEW.ID_sotrudnik),
+(select concat(position_name,', Оклад: ',cast(salary as varchar(30))) from position_s where id_position= NEW.dolzhnost_ID));
+end if; /*конец если*/
+end; /*конец*/
